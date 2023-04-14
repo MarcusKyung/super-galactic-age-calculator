df881395392df707936ffe493ad75ea791ce38a9
MarcusKyung <Kyungmj@gmail.com>
2023-04-14 15:04:39 -0700

    WIP: adds html form and UI logic.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import { Age } from './js/business-logic.js';
+
+
+
+
+
+
+
+function handleAgeForm() {
+  event.preventDefault();
+  const userAge = parseInt(document.querySelector('#currentAge').value);
+  const userAgePast = parseInt(document.querySelector('#pastAge').value);
+  const userAgeFuture = parseInt(document.querySelector('#futureAge').value);
+  const ageInput = new Age(userAge, userAgePast, userAgeFuture);
+  const mercuryAgeValue = ageInput.calculateMercury();
+  const venusAgeValue = ageInput.calculateVenus();
+  const marsAgeValue = ageInput.calculateMars();
+  const jupiterAgeValue = ageInput.calculateJupiter();
+  const pTag = document.createElement("p");
+  
+  pTag.append(`Your age on Earth is ${earthAgeValue}.`);
+  pTag.append(`Your age on Mercury is ${mercuryAgeValue}.`);
+  pTag.append(`Your age on Venus is ${venusAgeValue}.`);
+  pTag.append(`Your age on Mars is ${marsAgeValue}.`);
+  pTag.append(`Your age on Jupiter is ${jupiterAgeValue}.`);
+  
+  document.querySelector('#earthAge').append(pTag);
+  document.querySelector('#mercuryAge').append(pTag);
+  document.querySelector('#venusAge').append(pTag);
+  document.querySelector('#marsAge').append(pTag);
+  document.querySelector('#jupiterAge').append(pTag);
+
+
+window.addEventListener("load", function() {
+  document.querySelector("#ageInput").addEventListener("submit", handleAgeForm);
+});
